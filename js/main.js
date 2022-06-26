c21ffbb5ddf8a4157cd7e9202c9e449ac763c9dd
Allisson Lima <Allysoncosta@outlook.com>
2022-06-26 18:41:16 -0300

    feature: adicionando funcao para acionar classt jump e verificar se bateu no pipe

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe-obstacle')
+
+function jumpMario() {
+    mario.classList.add('jump-mario')
+    setTimeout(() => {
+        mario.classList.remove('jump-mario')
+    }, 500)
+}
+const onLoopCheck = setInterval(() => {
+    const pipeObstaclePosition = pipe.offsetLeft;
+    console.log(pipeObstaclePosition);
+    if (pipeObstaclePosition <= 120) {
+        pipe.style.animation = "none"
+        pipe.style.left = `${pipeObstaclePosition}px`
+    }
+}, 10)
+
+document.addEventListener('keydown', jumpMario)
