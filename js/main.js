b6a5e92c1a50d40bd06da9a6d0142fa9e754b2b6
Allisson Lima <Allysoncosta@outlook.com>
2022-06-26 20:47:32 -0300

    feature: adicionando validacao para musica do theme e game over

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+const source = "/assets/music/themeMusic.mp3";
+const audio = new Audio(); // use the constructor in JavaScript, just easier that way
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe-obstacle')
+const musicTheme = document.querySelector('.musicTheme')
+const musicGameOver = document.querySelector('.music-game-over')
 
+audio.src = source;
 function jumpMario() {
     mario.classList.add('jump-mario')
     setTimeout(() => {
@@ -8,19 +13,27 @@ function jumpMario() {
     }, 500)
 }
 const onLoopCheck = setInterval(() => {
+    audio.play();
     const pipeObstaclePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
     console.log(marioPosition);
     console.log(pipeObstaclePosition);
     if (pipeObstaclePosition <= 120 && marioPosition < 80 && pipeObstaclePosition > 0) {
+
+        audio.pause()
+        musicGameOver.play()
+
         pipe.style.animation = "none"
         pipe.style.left = `${pipeObstaclePosition}px`
 
         mario.style.animation = "none"
         mario.style.bottom = `${marioPosition}px`
+
         mario.src = "/assets/game-over.png"
         mario.style.width = "75px"
         mario.style.left = "50px"
+
+        clearInterval(onLoopCheck)
     }
 }, 10)
 
